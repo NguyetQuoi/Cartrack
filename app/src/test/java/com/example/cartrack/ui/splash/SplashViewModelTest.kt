e673ba8377357bcaed10d6ab76f1a9f71a7115e4
Quoi Tran <quoi.tran@zyrous.com>
2021-10-28 10:41:24 +0800

    fix dependencies for koin-test-junit5

---
+++ app/src/test/java/com/example/cartrack/ui/splash/SplashViewModelTest.kt
@@ -0,0 +1,18 @@
+package com.example.cartrack.ui.splash
+
+import com.example.cartrack.test.utils.InstantTaskExecutorExtension
+import com.example.cartrack.test.utils.TimingExtension
+import org.junit.jupiter.api.extension.ExtendWith
+import org.koin.test.KoinTest
+import org.koin.test.inject
+
+/**
+ * Unit tests for the [SplashViewModel].
+ * @author n.quoi
+ * @date 10.28.2021
+ */
+
+@ExtendWith(TimingExtension::class, InstantTaskExecutorExtension::class)
+class SplashViewModelTest: KoinTest {
+
+}
